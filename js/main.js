c1808196a1af207cf7a0394d528c40dcfe7b286f
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-12-01 17:24:37 +0100

    feat: add dynamically puzzle tiles

--- js/main.js
+++ js/main.js
@@ -1,2 +1,48 @@
 
+const img = [
+    './img/1.jpg',
+    './img/2.jpg',
+    './img/3.jpg',
+    './img/4.jpg',
+    './img/5.jpg',
+    './img/6.jpg',
+    './img/7.jpg',
+    './img/8.jpg',
+    './img/9.jpg',
+    './img/10.jpg',
+    './img/11.jpg',
+    './img/12.jpg',
+    './img/13.jpg',
+    './img/14.jpg',
+    './img/15.jpg',
+    './img/16.jpg',
+]
+
+
+
+const boardBox = document.getElementById('puzzle-board');
+
+const boardGame = document.getElementById('tiles-box');
+
+const reset = document.getElementById('reset');
+
+for (let i = 0; i < img.length; i++) {
+    boardGame.innerHTML += `
+    <div class="tiles">
+        <img src="${img[i]}">
+    </div>`
+}
+
+for (let i = 0; i < img.length; i++) {
+    boardBox.innerHTML += `
+    <div class="tiles-drag">
+    </div>`
+}
+
+
+
+
+
+
+
 
